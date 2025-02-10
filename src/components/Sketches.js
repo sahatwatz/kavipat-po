aa709b453d58f238f0fe980ca28526d0ab8f2536
sahatwatz <yoke.21@hotmail.com>
2025-02-10 13:55:02 +0700

    making responsive gallery

--- src/components/Sketches.js
+++ src/components/Sketches.js
@@ -1,13 +1,102 @@
-import React from "react";
+import { useState } from "react";
+import "../css/Sketches.css";
 
-const Sketches = () => {
+export default function Sketches() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
-    <div className="container">
-      <div>
-        <h1>Hello It's me Sketches Section</h1>
+    <div className="gallery-container">
+      <div
+        className="gallery-item"
+        onClick={() => setSelectedImage(require("../assets/img/pic1.jpg"))}
+      >
+        <img
+          src={require("../assets/img/pic1.jpg")}
+          alt="Thumbnail 1"
+          className="gallery-image"
+        />
       </div>
+      <div
+        className="gallery-item"
+        onClick={() => setSelectedImage(require("../assets/img/pic2.jpg"))}
+      >
+        <img
+          src={require("../assets/img/pic2.jpg")}
+          alt="Thumbnail 2"
+          className="gallery-image"
+        />
+      </div>
+      <div
+        className="gallery-item"
+        onClick={() => setSelectedImage(require("../assets/img/pic3.jpg"))}
+      >
+        <img
+          src={require("../assets/img/pic3.jpg")}
+          alt="Thumbnail 3"
+          className="gallery-image"
+        />
+      </div>
+
+      <div
+        className="gallery-item"
+        onClick={() => setSelectedImage(require("../assets/img/pic3.jpg"))}
+      >
+        <img
+          src={require("../assets/img/pic3.jpg")}
+          alt="Thumbnail 3"
+          className="gallery-image"
+        />
+      </div>
+
+      <div
+        className="gallery-item"
+        onClick={() => setSelectedImage(require("../assets/img/pic3.jpg"))}
+      >
+        <img
+          src={require("../assets/img/pic3.jpg")}
+          alt="Thumbnail 3"
+          className="gallery-image"
+        />
+      </div>
+
+      <div
+        className="gallery-item"
+        onClick={() => setSelectedImage(require("../assets/img/pic3.jpg"))}
+      >
+        <img
+          src={require("../assets/img/pic3.jpg")}
+          alt="Thumbnail 3"
+          className="gallery-image"
+        />
+      </div>
+      <div
+        className="gallery-item"
+        onClick={() => setSelectedImage(require("../assets/img/pic3.jpg"))}
+      >
+        <img
+          src={require("../assets/img/pic3.jpg")}
+          alt="Thumbnail 3"
+          className="gallery-image"
+        />
+      </div>
+      <div
+        className="gallery-item"
+        onClick={() => setSelectedImage(require("../assets/img/pic3.jpg"))}
+      >
+        <img
+          src={require("../assets/img/pic3.jpg")}
+          alt="Thumbnail 3"
+          className="gallery-image"
+        />
+      </div>
+
+      {selectedImage && (
+        <div className="popup-overlay" onClick={() => setSelectedImage(null)}>
+          <div className="popup-content">
+            <img src={selectedImage} alt="Selected" className="popup-image" />
+          </div>
+        </div>
+      )}
     </div>
   );
-};
-
-export default Sketches;
+}
